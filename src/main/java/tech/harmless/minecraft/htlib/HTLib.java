169201921a58f2c5db98703d42c2a241db81c9da
Harmless_Tech <2146578-harmless_tech@users.noreply.gitlab.com>
2020-12-26 21:02:58 -0500

    Initial Commit - HTLib

---
+++ src/main/java/tech/harmless/minecraft/htlib/HTLib.java
@@ -0,0 +1,13 @@
+package tech.harmless.minecraft.htlib;
+
+import net.fabricmc.api.ModInitializer;
+
+public class HTLib implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        System.out.println("Initializing HTLib...");
+        //TODO
+        System.out.println("Done initializing HTLib.");
+    }
+}
