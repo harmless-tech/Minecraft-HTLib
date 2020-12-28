009cc2b6208f4da1dcb68a6122eb67968de6481a
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2020-12-27 22:58:51 -0500

    Items now get added to the HTLib Overflow item group by default

--- src/main/java/tech/harmless/minecraft/htlib/lib/item/HTItem.java
+++ src/main/java/tech/harmless/minecraft/htlib/lib/item/HTItem.java
@@ -1,22 +1,39 @@
 package tech.harmless.minecraft.htlib.lib.item;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import org.atteo.classindex.IndexSubclasses;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import tech.harmless.minecraft.htlib.HTLib;
 
 @IndexSubclasses
 //TODO Add more functionality.
 public class HTItem extends Item {
 
-    protected String itemName;
-    protected ItemGroup itemGroup;
+    public String itemId;
 
-    public HTItem(@NotNull String itemName, @Nullable ItemGroup itemGroup, Settings settings) {
+    public HTItem(@NotNull String itemId) {
+        super(new FabricItemSettings().group(HTLib.ITEM_GROUP));
+
+        this.itemId = itemId;
+    }
+
+    public HTItem(@NotNull String itemId, @NotNull ItemGroup itemGroup) {
+        super(new FabricItemSettings().group(itemGroup));
+
+        this.itemId = itemId;
+    }
+
+    public HTItem(@NotNull String itemId, @NotNull Settings settings) {
         super(settings);
 
-        this.itemName = itemName;
-        this.itemGroup = itemGroup;
+        this.itemId = itemId;
+    }
+
+    public HTItem(@NotNull String itemId, @NotNull ItemGroup itemGroup, @NotNull Settings settings) {
+        super(settings.group(itemGroup));
+
+        this.itemId = itemId;
     }
 }
