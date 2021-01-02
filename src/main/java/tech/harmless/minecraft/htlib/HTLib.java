23d03edefe8c7b037d93cb8d2cd9da8dc7c5776b
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2021-01-01 21:22:34 -0500

    Version is now injected into manifest

--- src/main/java/tech/harmless/minecraft/htlib/HTLib.java
+++ src/main/java/tech/harmless/minecraft/htlib/HTLib.java
@@ -29,7 +29,7 @@ public class HTLib implements ModInitializer {
 
     public static final String NAME = "HTLib";
     public static final String ID = "htlib";
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = HTLib.class.getPackage().getImplementationVersion();
 
     public static final Logger LOG = LogManager.getLogger();
 
